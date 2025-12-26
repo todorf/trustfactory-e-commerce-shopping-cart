ef6dcbcdd0c77d560d1128c3559b423353e468e5
todorf <todor.filipovic18@gmail.com>
2025-12-26 18:02:38 +0100

    main - schedule the command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('sales:report-daily')
+    ->dailyAt('20:00')
+    ->timezone(config('app.timezone'))
+    ->description('Send daily sales report to admin user');
